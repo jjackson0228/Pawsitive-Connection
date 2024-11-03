63fa2385a1dd4478cfa61b19a7a6606aae6f5503
James Jackson <jjackson0228@gmail.com>
2024-11-03 12:00:16 -0500

    added seed data

---
+++ server/seeds/cleanDB.js
@@ -0,0 +1,25 @@
+/
+const models = require('../models');
+const db = require('../config/connection');
+
+module.exports = async (modelName, collectionName) => {
+  try {
+    // Check if the collection exists by listing collections in the database
+    const modelExists = await db.db
+      .listCollections({ name: collectionName })
+      .toArray();
+
+    // If the collection exists, drop it
+    if (modelExists.length > 0) {
+      await db.dropCollection(collectionName);
+      console.log(`Dropped collection: ${collectionName}`);
+    } else {
+      console.log(
+        `Collection ${collectionName} does not exist, skipping drop.`
+      );
+    }
+  } catch (err) {
+    console.error(`Error dropping collection ${collectionName}:`, err);
+    throw err;
+  }
+};
